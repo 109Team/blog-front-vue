4c81f08796b0dd77e688d5e0341d7843b764dd56
loohopen <loohopen@github.com>
2019-05-06 22:58:18 +0800

    第一次提交

---
+++ src/views/Post.vue
@@ -0,0 +1,17 @@
+<template>
+    <section>
+        <article>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nostrum minima totam explicabo temporibus perferendis atque eaque debitis quidem quia ipsa nemo sint ad, voluptatibus consequuntur voluptatem corrupti cupiditate ut.
+        </article>
+    </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Post extends Vue{
+    
+}
+</script>
+
---
+++ src/views/Posts.vue
@@ -0,0 +1,19 @@
+<template>
+    <ul>
+        <li>文章1</li>
+        <li>文章2</li>
+        <li>文章3</li>
+        <li>文章4</li>
+        <li>文章5</li>
+    </ul>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Posts extends Vue{
+    
+}
+</script>
+
