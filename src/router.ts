3e08f6d09cfe6cbd92c16371cbd3328f9cdee8fc
loohopen <loohopen@163.com>
2019-07-08 22:19:06 +0800

    v1.0.0 增加后台管理界面

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from '@/views/Home.vue';
-import Posts from '@/views/Posts.vue';
+import Home from '@/views/show/Home.vue';
+import Posts from '@/views/show/Posts.vue';
 
 Vue.use(Router);
 
@@ -21,26 +21,33 @@ export default new Router({
         {
           path: 'post/:postId',
           name: 'post',
-          component: () => import(/* webpackChunkName: "post" */ './views/Post.vue'),
+          component: () => import(/* webpackChunkName: "post" */ './views/show/Post.vue'),
         },
         {
           path: 'categrey',
           name: 'categrey',
-          component: () => import(/* webpackChunkName: "categrey" */ './views/Categrey.vue'),
+          component: () => import(/* webpackChunkName: "categrey" */ './views/show/Categrey.vue'),
         }
       ]
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      path: '/admin',
+      component: () => import(/* webpackChunkName: "categrey" */ './views/edit/Admin.vue'),
+      children: [
+        {
+          path: '',
+          name: 'postlist',
+          component: () => import(/* webpackChunkName: "categrey" */ './views/edit/PostList.vue'),
+        }
+      ]
     },
     {
       path: '/',
       redirect:'/home'
+    },
+    {
+      path: '/*',
+      redirect: '/home'
     }
   ],
 });
